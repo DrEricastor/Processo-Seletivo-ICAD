63d253c2cf4cd42a2d508c752a3f0eebab69f606
Ericastor <43832148+DrEricastor@users.noreply.github.com>
2025-04-01 16:22:05 -0300

    added tags as badges on task

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985d863cdc409bbf20484a6d1da963d6e8f0880e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e35cff9cbcbd06115c95c1128f037a98cdeb59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
